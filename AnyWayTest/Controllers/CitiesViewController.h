88bf2c42beeb29cf6d4740e649c116fa097163dd
kilovata <kilovata@gmail.com>
2015-04-17 00:27:40 +0300

    added CitiesVC (empty)

---
+++ AnyWayTest/Controllers/CitiesViewController.h
@@ -0,0 +1,13 @@
+//
+//  CitiesViewController.h
+//  AnyWayTest
+//
+//  Created by Sveta Kilovata on 17/04/15.
+//  Copyright (c) 2015 kilovata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CitiesViewController : UISearchController
+
+@end
