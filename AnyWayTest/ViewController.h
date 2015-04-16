787a46da8b5c712f9fcf92073301f2c37bc9b97d
kilovata <kilovata@gmail.com>
2015-04-16 23:04:41 +0300

    Initial Commit

---
+++ AnyWayTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnyWayTest
+//
+//  Created by Sveta Kilovata on 16/04/15.
+//  Copyright (c) 2015 kilovata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
